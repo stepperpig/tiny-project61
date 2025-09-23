173f8479cb2f768485250ac4d435c52fd92c7e0e
stepperpig <stepperpig@gmail.com>
2025-09-22 23:29:19 -0700

    added static files

---
+++ src/server/ngordnet_handler.py
@@ -0,0 +1,20 @@
+from aiohttp import web
+
+class QueryHandler:
+    def __init__(self):
+        pass
+
+    async def index_handler(request):
+        return web.FileResponse('./static/ngordnet_2a.html')
+
+    def _strtolist(self, s):
+        requested_words = s.split(',')
+        return requested_words
+
+    async def handle(self, request):
+        data = await request.get()
+        s = data.get('words')
+        words = self._strtolist(s)
+        startYear = data.get('startYear')
+        endYear = data.get('endYear')
+        return web.Response()
--- src/server/ngordnet_routes.py
+++ src/server/ngordnet_routes.py
@@ -1,4 +1,5 @@
 from server.ngordnet_views import index
+from server.ngordnet_handler import QueryHandler 
 
 def setup_routes(app):
     app.router.add_get('/', index)
